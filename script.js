3813ce2e7aac0433857e29a299f1bc7a82ba20ae
HartleyE1 <67687015+HartleyE1@users.noreply.github.com>
2020-07-02 09:24:58 +1000

    Update script.js
    fixed code line endings

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function sendMessage1() {
 
-    var contentText = document.getElementById('').value
-    var URL = document.getElementById('webhookURL').value
-    var username = document.getElementById('webhookUsername').value
-    var avatar = document.getElementById('webhookAvatar').value
+    var contentText = document.getElementById('').value;
+    var URL = document.getElementById('webhookURL').value;
+    var username = document.getElementById('webhookUsername').value;
+    var avatar = document.getElementById('webhookAvatar').value;
 
     var request = new XMLHttpRequest(URL);
     request.open("POST", "");
