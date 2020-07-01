b1f2cb3a9969441d71b44be186ef18033bb1ff9b
HartleyE1 <67687015+HartleyE1@users.noreply.github.com>
2020-07-01 22:51:04 +1000

    Edited Files

---
+++ script.js
@@ -0,0 +1,21 @@
+function sendMessage1() {
+
+    var contentText = document.getElementById('').value
+    var URL = document.getElementById('webhookURL').value
+    var username = document.getElementById('webhookUsername').value
+    var avatar = document.getElementById('webhookAvatar').value
+
+    var request = new XMLHttpRequest(URL);
+    request.open("POST", "");
+
+    request.setRequestHeader('Content-type', 'application/json');
+
+    var params1 = {
+      username: username,
+      avatar_url: avatar,
+      content: contentText
+
+    }
+
+    request.send(JSON.stringify(params1));
+  }
